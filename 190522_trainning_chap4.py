514d01f172165fbaa0c45599df76728bb92e77da
NHRD <naohisa.harada@gmail.com>
2019-05-22 20:02:05 +0900

    a

---
+++ 190522_trainning_chap4.py
@@ -0,0 +1,20 @@
+def fib(n):
+    a, b = 0,1
+    while a < n:
+        print(a, end = " ")
+        a, b = b, a + b
+    #print()
+
+fib(100)
+
+#a = 1000
+
+a = 100
+
+def test(n):
+    a = 10
+    n = n
+    print(n)
+
+print(a)
+test(10)
