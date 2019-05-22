253574accdbab908741f14e1d4a542ac7b4b91eb
NHRD <naohisa.harada@gmail.com>
2019-05-22 20:34:57 +0900

    a

--- 190522_trainning_chapt4_2.py
+++ 190522_trainning_chapt4_2.py
@@ -13,3 +13,33 @@ def ask_ok(prompt, retries=4, reminder='Please try again!'):
 
 result = ask_ok("Ok to quit??")
 print(result)
+
+def f(a, L=[]):
+    L = []
+    L.append(a)
+    return L
+
+print(f(1))
+print(f(2))
+print(f(3))
+
+def f(a, L=None):
+    if L is None:
+        L = []
+    L.append(a)
+    return L
+
+print(f(1))
+print(f(2))
+print(f(3))
+
+def parrot(voltage, state='a stiff', action='voom', type='Norwegian Blue'):
+    print("-- This parrot wouldn't", action, end=' ')
+    print("if you put", voltage, "volts through it.")
+    print("-- Lovely plumage, the", type)
+    print("-- It's", state, "!")
+
+parrot(1000)                                          # 1 positional argument
+parrot(voltage=1000)                                  # 1 keyword argument
+parrot(voltage=1000000, action='VOOOOOM')             # 2 keyword arguments
+parrot(voltage=1000000, action=100)             # 2 keyword arguments
