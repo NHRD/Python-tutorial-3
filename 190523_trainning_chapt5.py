2f5d7c9b74e84b29e29193e14af0cb6c8ea91496
NHRD <naohisa.harada@gmail.com>
2019-05-23 20:39:22 +0900

    a

--- 190523_trainning_chapt5.py
+++ 190523_trainning_chapt5.py
@@ -6,3 +6,19 @@ for n in range(2):
         #break
 
 print(sample)
+
+sample2 = [1, 1, 1, 2, 3, 2, 1]
+print(sample2.index(2, 1, 6))
+print(sample2.index(2, 4, 6))
+
+sample3 = [1, 2, 3]
+ext1 = (4, 5, 6)
+ext2 = {4:7, 5:8, 6:9}
+for b in range(2):
+    sample3.extend(ext1)
+    print(sample3)
+    sample3.extend(ext2)
+    print(sample3)
+    #if len(sample) >= 6:
+        #break
+
