38c624759e0321c409e5d68602af389c436e249f
NHRD <naohisa.harada@gmail.com>
2019-05-23 20:53:09 +0900

    a

--- 190523_trainning_chapt5_2.py
+++ 190523_trainning_chapt5_2.py
@@ -4,5 +4,7 @@ org_list = [2, 3, 1, 78, 9]
 org_list2 = sorted(org_list)
 org_list3 = org_list.sort()
 org_list.sort(reverse=True)
+org_list.reverse()
 
 print(org_list, "/n", org_list2, "/n", org_list3)
+
