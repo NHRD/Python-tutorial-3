4382fe19cca8914cf568bd11bf3b1de76f16b1d4
NHRD <naohisa.harada@gmail.com>
2019-05-23 20:26:08 +0900

    a

--- 190522_trainning_chapt4_3.py
+++ 190522_trainning_chapt4_3.py
@@ -7,3 +7,20 @@ concat("earth", "mars", "venus")
 pairs = [(1, 'one'), (2, 'two'), (3, 'three'), (4, 'four')]
 pairs.sort(key=lambda pair: pair[1]) #中のタプルの要素１、文字なのでアルファベット順にソート
 print(pairs)
+
+def test(n):
+    return lambda x:x+n
+
+result = test(10)
+print(result(10))
+print(result(10))
+print(result(0))
+
+def f(ham: int, eggs: int = 3) -> str:
+    print("Annotations:", f.__annotations__)
+    print("Arguments:", ham, eggs)
+    ham1 = str(ham)
+    eggs1 = str(eggs)
+    return ham1 + ' and ' + eggs1
+
+f(3)
---
+++ 190523_trainning_chapt5.py
@@ -0,0 +1,8 @@
+sample = [1, 2, 3,]
+ext = [1, 2, 3]
+for n in range(2):
+    sample.extend(ext)
+    #if len(sample) >= 6:
+        #break
+
+print(sample)
