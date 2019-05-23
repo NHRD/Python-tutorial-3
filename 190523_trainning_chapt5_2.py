686cc920109dea9372b6c702e29be92a4bbbb76f
NHRD <naohisa.harada@gmail.com>
2019-05-23 20:49:45 +0900

    a

---
+++ 190523_trainning_chapt5_2.py
@@ -0,0 +1,8 @@
+### EXILE PRIDE ###
+
+org_list = [2, 3, 1, 78, 9]
+org_list2 = sorted(org_list)
+org_list3 = org_list.sort()
+org_list.sort(reverse=True)
+
+print(org_list, "/n", org_list2, "/n", org_list3)
