38b85f4e6b0d786109362cfe48c21061e788321c
NHRD <naohisa.harada@gmail.com>
2019-05-24 19:12:05 +0900

    a

--- 190524_trainning_chapt5_4.py
+++ 190524_trainning_chapt5_4.py
@@ -3,3 +3,8 @@ print(a)
 
 b = {(3, 2): 3}
 print(b[(3, 2)])
+
+questions = ['name', 'quest', 'favorite color']
+answers = ['lancelot', 'the holy grail', 'blue']
+for q, a in zip(questions, answers):
+    print('What is your {}?  It is {}.'.format(q, a))
