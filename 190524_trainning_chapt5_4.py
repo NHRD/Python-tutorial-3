99d83874119c1b3d64096353f84dc09db003ce3e
NHRD <naohisa.harada@gmail.com>
2019-05-24 19:14:45 +0900

    a

--- 190524_trainning_chapt5_4.py
+++ 190524_trainning_chapt5_4.py
@@ -8,3 +8,9 @@ questions = ['name', 'quest', 'favorite color']
 answers = ['lancelot', 'the holy grail', 'blue']
 for q, a in zip(questions, answers):
     print('What is your {}?  It is {}.'.format(q, a))
+
+for i in reversed(range(0, 20, 2)):
+    print(i)
+
+ab = [i for i in reversed(range(0, 20, 2))]
+print(ab)
