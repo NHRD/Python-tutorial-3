d2fdc4992c2189f49ff6808deaf2917b08f9fc0c
NHRD <naohisa.harada@gmail.com>
2019-05-25 11:25:22 +0900

    a

--- 190525_trainning_chapt10.py
+++ 190525_trainning_chapt10.py
@@ -1,8 +1,21 @@
 import statistics
 
 data = [0, 1, 2, 3, 4, 5, 6]
-print(statistics.mean(data))     #平均値
-print(statistics.median(data))　 #中央値
-print(statistics.variance(data)) #分散
 
+print(statistics.mean(data))
+print(statistics.variance(data))
 
+from urllib.request import urlopen
+with urlopen("http://tycho.usno.navy.mil/cgi-bin/timer.pl") as response:
+    for line in response:
+        line = line.decode("utf-8")
+        if "EST" in line or "EDT" in line:
+            print(line)
+
+from datetime import date
+
+now = date.today()
+birthday = date(1910, 5, 10)
+age = now - birthday
+print(age.days)
+print(int(age.days)//365)
