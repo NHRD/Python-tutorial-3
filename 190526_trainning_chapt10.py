120d9d400146cd962bd0bc53684f843d0b5e7234
NHRD <naohisa.harada@gmail.com>
2019-05-26 15:16:48 +0900

    a

--- 190526_trainning_chapt10.py
+++ 190526_trainning_chapt10.py
@@ -4,4 +4,11 @@ dic = [
     ['Matsumoto', 'Tanaka', 'Ivan'],
 ]
 
-print(list(zip(*dic)))
+print(list(zip(dic)))
+
+print(list(zip([1, 2, 3], [2, 3, 4])))
+
+from string import Template
+
+sam = Template("$ss is $dd")
+print(sam.substitute(ss = "a", dd = "b"))
